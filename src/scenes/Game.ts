8edf1a24bc72360df8977a71e5037c99bda1848e
Philiks <janopol.felixjr@gmail.com>
2022-08-14 07:24:50 +0800

    Initial setup using the official logo of `CIP`.

--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -6,11 +6,21 @@ export default class Demo extends Phaser.Scene {
   }
 
   preload() {
-    this.load.image('logo', 'assets/phaser3-logo.png');
+    this.load.image('logo', 'assets/cip-logo.png');
+    this.load.image('fire', 'http://labs.phaser.io/assets/particles/fire1.png');
   }
 
   create() {
+    const particle = this.add.particles('fire');
+    const emitter = particle.createEmitter({
+      speed: 100,
+      scale: { start: 1, end: 0 },
+      blendMode: 'ADD'
+    });
+
     const logo = this.add.image(400, 70, 'logo');
+    logo.setScale(0.25);
+    emitter.startFollow(logo);
 
     this.tweens.add({
       targets: logo,
