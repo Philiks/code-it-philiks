0da50125e4dd4ae4bbbc54e46451d0fdbbd54b9a
Philiks <janopol.felixjr@gmail.com>
2022-08-27 02:58:46 +0800

    [Improved] Documentation. [Refactor] Added `rowPosition` for the `InstructionBlockList`.

--- src/gameObjects/blocks/DestinationBlock.ts
+++ src/gameObjects/blocks/DestinationBlock.ts
@@ -2,21 +2,55 @@ import Block from "./Block";
 import SourceBlock from "./SourceBlock";
 
 export default class DestinationBlock extends Block {
+  /**
+   * The row position of the `DestinationBlock` on the list.
+   * This starts at 1 and **NOT** 0. Since this will be used
+   * by `LevelScene.instructionBlockList` where the first
+   * index [0] is reserved for the `StartBlock`.
+   */
+  readonly rowPosition!: number;
+
+  /**
+   * The *Drop Target* that will trigger `drag*` events.
+   *
+   * i.e. `dragEnter` event triggers when a `SourceBlock`
+   * enters this `DestinationBlock`.
+   *
+   * Access via *(getter)* `dropZoneObj`.
+   */
   private _dropZoneObj!: Phaser.GameObjects.Zone;
+
+  /**
+   * The `SourceBlock` instance that occupies this
+   * `DestinationBlock`.
+   *
+   * Access via *(getter)* `occupiedBy`.\
+   * Mutate via *(setter)* `occupiedBy()`.
+   */
   private _occupiedBy?: SourceBlock;
 
-  constructor(scene: Phaser.Scene, x: number, y: number) {
+  /**
+   * Creates a `Container`|`DestinationBlock` that contains different
+   * `Phaser.GameObjects`.
+   * @param scene The `Phaser.Scene` that this `Container` will belong to.
+   * @param x The horizontal position of this block.
+   * @param y The vertical position of this block.
+   * @param rowPosition The row position of this block. Starts at 1.
+   */
+  constructor(scene: Phaser.Scene, x: number, y: number, rowPosition: number) {
     super(scene, x, y);
+    this.rowPosition = rowPosition;
   }
 
   /**
    * Calls the `this.initObjects()`. Texture is not needed as a
    * parameter since each derived classes has their own `texture`.
+   *
    * While the *code* or `text` that will be put on that block will
    * depend on the level design.
    * @param text The *code* that the block contains.
    */
-  create(text: string): void {
+  public create(text: string): void {
     this.initObjects("destination_block", text);
 
     this._dropZoneObj = this.scene.add
@@ -25,7 +59,7 @@ export default class DestinationBlock extends Block {
     this.add(this._dropZoneObj);
   }
 
-  update(): void {
+  public update(): void {
     // TODO: Animation.
   }
 
