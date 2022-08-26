bf620e8b8b2d9646de6a8277540a7711964c5a46
Philiks <janopol.felixjr@gmail.com>
2022-08-27 02:58:26 +0800

    [Refactor] Derived from `LevelScene` and defines the blocks for **Level One**.

--- src/scenes/LevelOneScene.ts
+++ src/scenes/LevelOneScene.ts
@@ -1,174 +1,108 @@
-import Phaser from "phaser";
-
 import DestinationBlock from "../gameObjects/blocks/DestinationBlock";
 import StartBlock from "../gameObjects/blocks/StartBlock";
 import EndBlock from "../gameObjects/blocks/EndBlock";
 import SourceBlock from "../gameObjects/blocks/SourceBlock";
 
-// TODO: Create multiple levels. Change this to LevelOne after.
-export default class LevelScene extends Phaser.Scene {
+import LevelScene from "./LevelScene";
+
+export default class LevelOneScene extends LevelScene {
   constructor() {
-    super("LevelScene");
+    super("LevelOneScene");
   }
 
-  preload(): void {
-    this.load.path = "assets/blocks/";
-    this.load.image("start_block", "start_block.png");
-    this.load.image("end_block", "end_block.png");
-    this.load.image("source_block", "source_block.png");
-    this.load.image("destination_block", "destination_block.png");
+  protected initBlocks(): void {
+    // Start Block.
+    this.startBlock = new StartBlock(this, 200, 50);
+    this.startBlock.create("int x = 0, a, b;");
+
+    // Destination Block.
+    this.destinationBlocks.push(
+      new DestinationBlock(this, 200, 150, 1),
+      new DestinationBlock(this, 200, 225, 2),
+      new DestinationBlock(this, 200, 300, 3)
+    );
+
+    this.destinationBlocks.forEach((destinationBlock: DestinationBlock) =>
+      destinationBlock.create("// Drag a Source Block here.")
+    );
+
+    // Source Block.
+    this.sourceBlocks.push([
+      new SourceBlock(this, 450, 150),
+      new SourceBlock(this, 675, 150),
+    ]);
+
+    this.sourceBlocks.push([
+      new SourceBlock(this, 450, 225),
+      new SourceBlock(this, 675, 225),
+    ]);
+
+    this.sourceBlocks.push([
+      new SourceBlock(this, 450, 300),
+      new SourceBlock(this, 675, 300),
+    ]);
+
+    this.sourceBlocks[0][0].create("int a = 3;");
+    this.sourceBlocks[0][1].create("int b = 5;");
+
+    this.sourceBlocks[1][0].create("x = a;");
+    this.sourceBlocks[1][1].create("x = b;");
+
+    this.sourceBlocks[2][0].create("x = x + a;");
+    this.sourceBlocks[2][1].create("x = x + b;");
+
+    // End Block.
+    this.endBlock = new EndBlock(this, 200, 400);
+    this.endBlock.create("x = 10;");
   }
 
-  create(): void {
-    // Start
-    const startBlock = new StartBlock(this, 200, 50);
-    startBlock.create("int x = 0;");
+  protected initVariables(): void {
+    // Start Block.
+    this.startBlock.variableActionCallback = () => {
+      this.variables = {
+        varX: 0,
+        varA: null,
+        varB: null,
+      };
+    };
 
-    // Destination
-    const firstDestinationBlock = new DestinationBlock(this, 200, 150);
-    firstDestinationBlock.create("// Drag a Source Block here.");
+    // Source Block.
+    this.sourceBlocks[0][0].variableActionCallback = () =>
+      (this.variables.varA = 3);
+    this.sourceBlocks[0][1].variableActionCallback = () =>
+      (this.variables.varB = 5);
 
-    const secondDestinationBlock = new DestinationBlock(this, 200, 225);
-    secondDestinationBlock.create("// Drag a Source Block here.");
+    this.sourceBlocks[1][0].variableActionCallback = () =>
+      (this.variables.varX = this.variables.varA);
+    this.sourceBlocks[1][1].variableActionCallback = () =>
+      (this.variables.varX = this.variables.varB);
 
-    const thirdDestinationBlock = new DestinationBlock(this, 200, 300);
-    thirdDestinationBlock.create("// Drag a Source Block here.");
+    this.sourceBlocks[2][0].variableActionCallback = () =>
+      (this.variables.varX += this.variables.varA);
+    this.sourceBlocks[2][1].variableActionCallback = () =>
+      (this.variables.varX += this.variables.varB);
 
-    // Source
-    const firstSourceBlockLeft = new SourceBlock(this, 450, 150);
-    firstSourceBlockLeft.create("int a = 10;");
-
-    const firstSourceBlockRight = new SourceBlock(this, 675, 150);
-    firstSourceBlockRight.create("int b = 5;");
-
-    const secondSourceBlockLeft = new SourceBlock(this, 450, 225);
-    secondSourceBlockLeft.create("x = a;");
-
-    const secondSourceBlockRight = new SourceBlock(this, 675, 225);
-    secondSourceBlockRight.create("x = b;");
-
-    const thirdSourceBlockLeft = new SourceBlock(this, 450, 300);
-    thirdSourceBlockLeft.create("x = x + a;");
-
-    const thirdSourceBlockRight = new SourceBlock(this, 675, 300);
-    thirdSourceBlockRight.create("x = x + b;");
-
-    // End
-    const endBlock = new EndBlock(this, 200, 400);
-    endBlock.create("x = 10;");
-
-    // Register SourceBlocks to their DestinationBlock.
-    firstSourceBlockLeft.destinationBlock = firstDestinationBlock;
-    firstSourceBlockRight.destinationBlock = firstDestinationBlock;
-    secondSourceBlockLeft.destinationBlock = secondDestinationBlock;
-    secondSourceBlockRight.destinationBlock = secondDestinationBlock;
-    thirdSourceBlockLeft.destinationBlock = thirdDestinationBlock;
-    thirdSourceBlockRight.destinationBlock = thirdDestinationBlock;
-
-    this.input
-      .on(
-        "drag",
-        (
-          pointer: Phaser.Input.Pointer,
-          gameObject: SourceBlock,
-          dragX: number,
-          dragY: number
-        ): void => {
-          gameObject.setPosition(dragX, dragY);
-        }
-      )
-      .on(
-        "dragstart",
-        (pointer: Phaser.Input.Pointer, gameObject: SourceBlock): void => {
-          gameObject.bgImageObj.setTint(0x00bb00);
-        }
-      )
-      .on(
-        "dragend",
-        (
-          pointer: Phaser.Input.Pointer,
-          gameObject: SourceBlock,
-          dropped: boolean
-        ): void => {
-          if (!dropped)
-            gameObject.setPosition(
-              gameObject.input.dragStartX,
-              gameObject.input.dragStartY
-            );
-          gameObject.bgImageObj.clearTint();
-        }
-      )
-      .on(
-        "dragenter",
-        (
-          pointer: Phaser.Input.Pointer,
-          gameObject: SourceBlock,
-          target: Phaser.GameObjects.Zone
-        ): void => {
-          const destinationContainer =
-            target.parentContainer as DestinationBlock;
-          if (gameObject.destinationBlock === destinationContainer) {
-            // Preview the original position of the occupying SourceBlock.
-            if (destinationContainer.occupiedBy)
-              destinationContainer.occupiedBy.resetPosition();
-
-            destinationContainer.bgImageObj.setTint(0x00bb00);
-          }
-        }
-      )
-      .on(
-        "dragleave",
-        (
-          pointer: Phaser.Input.Pointer,
-          gameObject: SourceBlock,
-          target: Phaser.GameObjects.Zone
-        ): void => {
-          const destinationContainer =
-            target.parentContainer as DestinationBlock;
-          if (gameObject.destinationBlock === destinationContainer) {
-            // Preview the original position of the occupying SourceBlock.
-            if (destinationContainer.occupiedBy)
-              destinationContainer.occupiedBy.setPosition(
-                destinationContainer.x,
-                destinationContainer.y
-              );
-
-            destinationContainer.bgImageObj.clearTint();
-          }
-        }
-      )
-      .on(
-        "drop",
-        (
-          pointer: Phaser.Input.Pointer,
-          gameObject: SourceBlock,
-          target: Phaser.GameObjects.Zone
-        ): void => {
-          const destinationContainer =
-            target.parentContainer as DestinationBlock;
-          if (gameObject.destinationBlock === destinationContainer) {
-            // Return the occupying SourceBlock to its original position.
-            if (destinationContainer.occupiedBy)
-              destinationContainer.occupiedBy.resetPosition();
-
-            gameObject.setPosition(
-              destinationContainer.x,
-              destinationContainer.y
-            );
-            destinationContainer.occupiedBy = gameObject;
-
-            // TODO: Identify whether it is correct or not. Decide whether to have an internal parser or not.
-          } else {
-            gameObject.setPosition(
-              gameObject.input.dragStartX,
-              gameObject.input.dragStartY
-            );
-          }
-        }
-      );
+    // End Block.
+    this.endBlock.variableActionCallback = () => {
+      const text = this.variables.varX === 10 ? "SUCCESS" : "FAILED";
+      this.add
+        .text(this.cameras.main.centerX, this.cameras.main.centerY, text, {
+          font: "bold 20px Arial",
+          color: "black",
+          resolution: 3,
+        })
+        .setOrigin(0.5);
+    };
   }
 
-  update(): void {
-    // TODO: Animation.
+  protected registerSrcBlkToDestBlk(): void {
+    this.sourceBlocks.forEach(
+      (rowSourceBlocks: SourceBlock[], destinationIndex: number) =>
+        rowSourceBlocks.forEach(
+          (sourceBlock: SourceBlock) =>
+            (sourceBlock.destinationBlock =
+              this.destinationBlocks[destinationIndex])
+        )
+    );
   }
 }
